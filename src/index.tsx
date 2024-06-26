469bb28e333e9fe939dea5064c2e9e790d87519f
Eastyn <eastynwork@gmail.com>
2024-06-26 18:55:59 +0900

    fight with install errors and I won

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 const darkTheme = {
 	textColor: "#fcfcfc",
