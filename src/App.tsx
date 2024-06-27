8007418bb0899033e7f0e44f3bfadf9bcf5b7931
Eastyn <eastynwork@gmail.com>
2024-06-27 18:56:44 +0900

    interface is an explanation for an object

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,11 @@
 import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-	color: ${props => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	height: 100vh;
-	background-color: ${props => props.theme.backgroundColor};
-`;
-
-const rotating = keyframes`
-	0% {
-		transform: rotate(0deg);
-		border-radius: 0;
-	}
-	50% {
-		border-radius: 100%;
-	}
-	100%{
-		transform: rotate(360deg);
-	}
-`;
-
-const Emoji = styled.span`
-	font-size: 36px;
-`;
-
-const Box = styled.div`
-	height: 200px;
-	width: 200px;
-	background-color: yellowgreen;
-	animation: ${rotating} 1s linear infinite;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	${Emoji} {
-		&:hover {
-			font-size: 98px;
-		}
-	}
-`;
-
+import Circle from "./Circle";
 function App() {
 	return (
-		<Wrapper>
-			<Title>This is a title</Title>
-			<Box>
-				<Emoji as="p">😎</Emoji>
-			</Box>
-			<Emoji>❤️‍🔥</Emoji>
-		</Wrapper>
+		<div>
+			<Circle bgColor="teal" />
+			<Circle bgColor="skyblue" />
+		</div>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-const darkTheme = {
-	textColor: "#fcfcfc",
-	backgroundColor: "#222",
-};
-
-const lightTheme = {
-	backgroundColor: "#fcfcfc",
-	textColor: "#222",
-};
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={darkTheme}>
-			<App />
-		</ThemeProvider>
+		<App />
 	</React.StrictMode>
 );
