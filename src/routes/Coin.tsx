5dc86029e24f03f8744d43a6ffc59661e9e5569f
Eastyn <eastynwork@gmail.com>
2024-07-01 18:54:13 +0900

    set interface for coin info and coin price info

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -31,11 +31,79 @@ interface IRouteState {
 	};
 }
 
+interface ICoinInfo {
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	is_new: boolean;
+	is_active: boolean;
+	type: string;
+	description: string;
+	message: string;
+	open_source: boolean;
+	started_at: string;
+	development_status: string;
+	hardware_wallet: boolean;
+	proof_type: string;
+	org_structure: string;
+	hash_algorithm: string;
+	first_data_at: string;
+	last_data_at: string;
+}
+
+interface IPriceInfo {
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	total_supply: number;
+	max_supply: number;
+	beta_value: number;
+	first_data_at: string;
+	last_updated: string;
+	quotes: {
+		USD: {
+			price: number;
+			volume_24h: number;
+			volume_24h_change_24h: number;
+			market_cap: number;
+			market_cap_change_24h: number;
+			percent_change_15m: number;
+			percent_change_30m: number;
+			percent_change_1h: number;
+			percent_change_6h: number;
+			percent_change_12h: number;
+			percent_change_24h: number;
+			percent_change_7d: number;
+			percent_change_30d: number;
+			percent_change_1y: number;
+			ath_price: number;
+			ath_date: string;
+			percent_from_price_ath: number;
+		};
+	};
+}
+
 function Coin() {
 	const [isLoading, setIsLoading] = useState(true);
 	const { coinId } = useParams<string>();
-	const { state } = useLocation() as IRouteState;
+	const { state } = useLocation() as IRouteState; // react-router-dom v6 에서는 제너릭 지원 안함
+	const [info, setInfo] = useState<ICoinInfo>();
+	const [priceInfo, setPriceInfo] = useState<IPriceInfo>();
 
+	useEffect(() => {
+		const getCoinData = async () => {
+			const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+
+			const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+			setInfo(infoData);
+			setPriceInfo(priceData);
+			setIsLoading(false);
+		};
+		getCoinData();
+	}, []);
+	console.log(info, priceInfo);
 	return (
 		<Container>
 			<Header>
