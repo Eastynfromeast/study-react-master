55afd768f5772a275d31f37cca750550df83e9f2
Eastyn <eastynwork@gmail.com>
2024-07-02 23:26:00 +0900

    make a recap of how to use react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
 	const [toDo, setToDo] = useState("");
 	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
 		const {
@@ -16,6 +17,25 @@ function ToDoList() {
 		<div>
 			<form onSubmit={onSubmit}>
 				<input value={toDo} onChange={onChange} placeholder="Write a to do" type="text" />
+				<button>Add</button>
+			</form>
+		</div>
+	);
+} */
+
+function ToDoList() {
+	const { register, watch } = useForm();
+	console.log(watch());
+
+	return (
+		<div>
+			<form>
+				<input {...register("email")} placeholder="Email" />
+				<input {...register("firstName")} placeholder="First Name" />
+				<input {...register("lastName")} placeholder="Last Name" />
+				<input {...register("userName")} placeholder="User Name" />
+				<input {...register("password")} placeholder="Password" />
+				<input {...register("pwValidation")} placeholder="Password Validation" />
 				<button>Add</button>
 			</form>
 		</div>
