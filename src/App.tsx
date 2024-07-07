c647b9b88e82ea846b0588d5c48b0159b9d048fd
Eastyn <eastynwork@gmail.com>
2024-07-07 21:23:05 +0900

    framer motion allows you control children, so check the orchestration part of animation

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,8 @@ table {
 
 body {
 	font-family: "Source Sans 3", sans-serif;
-	background-color: ${props => props.theme.bgColor};
-	color: ${props => props.theme.textColor};
+	background-color: ${props => props.theme.textColor};
+	color: ${props => props.theme.bgColor};
 }
 
 a {
@@ -67,26 +67,74 @@ a {
 }
 `;
 
+const Wrapper = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #b938f7;
+`;
+
 const Box = styled(motion.div)`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
 	width: 200px;
 	height: 200px;
-	background-color: #fff;
-	border-radius: 15px;
+	background-color: rgba(255, 255, 255, 0.2);
+	border-radius: 40px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-	initial: { scale: 0 },
-	animate: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+const Circle = styled(motion.div)`
+	background: #fff;
+	height: 70px;
+	width: 70px;
+	place-self: center;
+	border-radius: 100%;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+	font-size: 0;
+`;
+
+const boxVariants = {
+	start: {
+		opacity: 0,
+		scale: 0.5,
+	},
+	end: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			type: "spring",
+			duration: 2,
+			bounce: 0.5,
+			delayChildren: 0.5,
+			staggerChildren: 0.5,
+		},
+	},
+};
+
+const circleVariants = {
+	start: {
+		opacity: 0,
+	},
+	end: {
+		opacity: 1,
+	},
 };
 
 function App() {
 	return (
 		<>
 			<GlobalStyle />
-			<Box variants={myVars} initial="initial" animate="animate">
-				Box
-			</Box>
+			<Wrapper>
+				<Box variants={boxVariants} initial="start" animate="end">
+					<Circle variants={circleVariants} />
+					<Circle variants={circleVariants} />
+					<Circle variants={circleVariants} />
+					<Circle variants={circleVariants} />
+				</Box>
+			</Wrapper>
 		</>
 	);
 }
