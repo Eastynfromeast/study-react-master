f3f1c1b15d3ccc475c130c72ae193c41935b347d
Eastyn <eastynwork@gmail.com>
2024-07-09 23:14:50 +0900

    When a new component is added that has a layoutId prop that matches an existing component, it will automatically animate out from the old component.

--- src/App.tsx
+++ src/App.tsx
@@ -74,11 +74,21 @@ const Wrapper = styled(motion.div)`
 	display: flex;
 	justify-content: space-around;
 	align-items: center;
-	flex-direction: column;
 	background: linear-gradient(135deg, rgb(131, 188, 217), rgb(0, 79, 238));
 `;
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	width: 50vw;
+	gap: 10px;
+	div:first-child,
+	div:last-child {
+		grid-column: span 2;
+	}
+`;
+
 const Box = styled(motion.div)`
-	width: 400px;
 	height: 400px;
 	background-color: rgba(255, 255, 255, 1);
 	border-radius: 40px;
@@ -88,23 +98,42 @@ const Box = styled(motion.div)`
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-	background-color: #00a5ff;
-	width: 100px;
-	height: 100px;
-	border-radius: 50px;
-	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+	width: 100%;
+	height: 100%;
+
+	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
 function App() {
-	const [isClicked, setIsClicked] = useState(false);
-	const toggleIsClicked = () => setIsClicked(prev => !prev);
+	const [id, setId] = useState<null | string>();
+	const closeOverlay = () => {
+		setId(null);
+	};
 	return (
 		<>
 			<GlobalStyle />
-			<Wrapper onClick={toggleIsClicked}>
-				<Box>{!isClicked ? <Circle layoutId="circle" style={{ borderRadius: "50px" }} /> : null}</Box>
-				<Box>{isClicked ? <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} /> : null}</Box>
+			<Wrapper>
+				<Grid>
+					{["1", "2", "3", "4"].map(n => (
+						<Box key={n} layoutId={n} onClick={() => setId(n)} />
+					))}
+				</Grid>
+				<AnimatePresence>
+					{id ? (
+						<Overlay
+							initial={{ backgroundColor: "rgba(0,0,0,0)" }}
+							animate={{ backgroundColor: "rgba(0,0,0,.5)" }}
+							exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+							onClick={closeOverlay}
+						>
+							<Box layoutId={id} style={{ width: "50vw", height: 400 }} />
+						</Overlay>
+					) : null}
+				</AnimatePresence>
 			</Wrapper>
 		</>
 	);
